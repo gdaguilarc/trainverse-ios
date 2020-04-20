78077bb098c99b3c6956bf42af382ea7a3dc98a4
David Aguilar <gd.aguilarc@gmail.com>
2020-04-20 16:04:38 -0400

    Base project / storyboard boilerplate

---
+++ trainverse/trainverse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trainverse
+//
+//  Created by user167991 on 4/20/20.
+//  Copyright © 2020 itesm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
